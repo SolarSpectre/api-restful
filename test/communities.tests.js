11e29a93b6d1454a8db16e673493f15f1110257d
SolarSpectre <josephcaza32@gmail.com>
2024-11-30 16:40:01 -0500

    pruebas unitarias y de carga parte 1

--- test/communities.tests.js
+++ test/communities.tests.js
@@ -1,12 +1,14 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../src/server.js');
+import {default as chaiHttp, request} from "chai-http";
+import * as chai from "chai";
+import app from '../src/server.js';
 
 chai.use(chaiHttp);
-const { expect } = chai;
-
+const {expect} = chai;
 describe('GET /api/communities', () => {
   it('Debe devolver una lista de comunidades', async () => {
-    
+    const res = await request.execute(app).get('/api/communities');
+    expect(res).to.have.status(200);
+    expect(res.body).to.be.an('array');
+    expect(res.body).to.have.length(1);
   });
 });
