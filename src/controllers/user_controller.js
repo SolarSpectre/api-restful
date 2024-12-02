4f3c8dd844495c94656b26fa6161acbe3b49569e
SolarSpectre <josephcaza32@gmail.com>
2024-12-02 12:25:22 -0500

    add profile picture functionality

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,6 +1,8 @@
 import userModel from "../models/user.js"
 import { v4 as uuidv4 }  from 'uuid'
 import bcrypt from "bcrypt"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs-extra";
 const saltRounds = 10
 import {createToken} from '../middlewares/auth.js';
 
@@ -15,6 +17,12 @@ const registerUser = async(req, res)=> {
         ...otherDataUser
     }
     try {
+        if (req.files?.imagen) {
+            const cloudinaryResponse = await cloudinary.uploader.upload(req.files.imagen.tempFilePath, { folder: "users" });
+            userData.imagen = cloudinaryResponse.secure_url;
+            userData.public_id = cloudinaryResponse.public_id;
+            await fs.unlink(req.files.imagen.tempFilePath);
+        }
         const user = await userModel.registerUserModel(userData)
         res.status(200).json(user)
     } catch (error) {
